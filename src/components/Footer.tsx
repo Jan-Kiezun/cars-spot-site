2c015bbf14bf7f2f22faac8b97f8db37ac46c4b2
jan_kiezun <j4n3k777@gmail.com>
2024-09-15 17:20:04 +0200

    added footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,14 @@
 ﻿function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div
+      className="bg-[#282828] text-[#f7f7f7] py-12 flex flex-row gap-24 px-24 items-center justify-between font-roboto-condensed
+      max-sm:px-4 max-sm:flex-col max-sm:py-8 max-sm:justify-start max-sm:items-start max-sm:gap-5 max-sm:pb-8
+    "
+    >
+      <span className="font-extrabold text-xl font-roboto-flex">Cars Spot</span>
+      <span className="underline font-semibold">Polityka prywatności</span>
+    </div>
+  );
 }
 
 export default Footer;
