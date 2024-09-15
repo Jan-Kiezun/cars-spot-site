ab87be91855a0d7b85c1c2155a83e7cbb7f9146b
jan_kiezun <j4n3k777@gmail.com>
2024-09-15 16:45:50 +0200

    added logic to choosing cars

--- src/components/HeadersBlock.tsx
+++ src/components/HeadersBlock.tsx
@@ -1,8 +1,19 @@
-﻿function HeadersBlock() {
-  // blok z niebieskim tekstem  Prezentacja firmy
-  // większym czarnym z czcionką Roboto Flex: Zobacz naszą galerię zdjęć
-  // i dwoma tekstami które stają się niebieskie on focus: Samochody osobowe, Samochody dostawcze
-  // tło ma byc takie same
+﻿import { useState } from "react";
+
+function HeadersBlock() {
+  const [activePersonalCars, setActivePersonalCars] = useState(true);
+  const [activeDeliveryCars, setActiveDeliveryCars] = useState(false);
+
+  const handlePersonalCars = () => {
+    setActivePersonalCars(true);
+    setActiveDeliveryCars(false);
+  };
+
+  const handleDeliveryCars = () => {
+    setActivePersonalCars(false);
+    setActiveDeliveryCars(true);
+  };
+
   return (
     <div className="flex flex-col mx-36 max-lg:mx-12 max-sm:mx-6 mt-24 max-lg:mt-72 max-sm:mt-24 ">
       <h2 className="z-10 text-[#0147FF] text-lg">Prezentacja firmy</h2>
@@ -12,14 +23,22 @@
         </h1>
         <div className="flex flex-row gap-6">
           <button
-            className="font-roboto-condensed font-semibold  tracking-tight mb-2 focus:mb-0
-          focus:text-[#0136DF] focus:outline-none focus:border-b-2 border-[#0136DF]"
+            className={`font-roboto-condensed font-semibold  tracking-tight mb-2
+              ${
+                activePersonalCars ? "text-[#0136DF] border-b-2 mb-0" : ""
+              } focus:outline-none focus:
+          border-[#0136DF]`}
+            onClick={handlePersonalCars}
           >
             Samochody osobowe
           </button>
           <button
-            className="font-roboto-condensed font-semibold tracking-tight mb-2 focus:mb-0
-          focus:text-[#0136DF] focus:outline-none focus:border-b-2 border-[#0136DF]"
+            className={`font-roboto-condensed font-semibold  tracking-tight mb-2
+              ${
+                activeDeliveryCars ? "text-[#0136DF] border-b-2 mb-0" : ""
+              } focus:outline-none focus:
+              border-[#0136DF]`}
+            onClick={handleDeliveryCars}
           >
             Samochody dostawcze
           </button>
